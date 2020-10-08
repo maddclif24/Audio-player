f74e4f8933605bf6a0a7a76db9473329f6f7cb10
maddclif <maddclif24@gmail.com>
2020-10-08 18:22:27 +0300

    new html to module

---
+++ newHtml.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const tracks = fs.readdirSync('public/tracks');
+const nameTracks = tracks.map((item) => item.slice(0, -4));
+const trackList = nameTracks.reduce((x, y) => x.includes(y) ? x : [...x, y], []);
+const list = `<ol id="trackList">${trackList.map((item) => `<li><a onclick="chooseTrack(event)" href="#">${item}</a></li>`).join('')}</ol>`;
+
+export const html = `
+<!DOCTYPE html>
+  <html lang="">
+      <head>
+      <meta charset="UTF-8">
+      <title>Title</title>
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/f5792cc639.js" crossorigin="anonymous"></script>
+      </head>
+      <body>
+      <h1>Audio Player</h1>
+      <div id="player-block">
+      <audio id="player" controls>
+      <source id="ogg" src="tracks/jazzfrenchy.ogg" type="audio/ogg">
+      <source id="mp3" src="tracks/jazzfrenchy.mp3" type="audio/mp3">
+      Ваш браузер не пожжерживает тег audio!
+  </audio>
+  <button class="btn" onclick="getNextTrack()" id="Next"><i class="fas fa-fast-forward fa-1x"></i></button>
+  <div id="current-track"></div>
+  <div id="trackSelector">${list}</div>
+        <form method='post' action='upload' enctype="multipart/form-data">
+        <input class="loadBtn" type='file' name='fileUploaded' id="browse">
+        <input class="loadBtn" type='submit' id="upload" value="Upload">
+      </div>
+      <script src="showCurrentTrack.js"></script>
+      <script src="skipTrack.js"></script>
+      <script src="trackSelection.js"></script>
+      </body>
+      </html>`;
